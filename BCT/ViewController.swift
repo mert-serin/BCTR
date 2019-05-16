92e76b8648baeb257000befab0d43509fc0ffe9b
Mert <mert-serin@outlook.com>
2019-05-16 10:16:03 +0300

    Initial Commit

---
+++ BCT/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BCT
+//
+//  Created by Mert Serin on 16.05.2019.
+//  Copyright © 2019 Mert Serin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
